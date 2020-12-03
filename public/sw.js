de11a6a86832b330dd634a508b57a10f3eaa4099
Jarom Plewe <jplewe23@gmail.com>
2020-12-02 23:00:11 -0700

    added fetch listener

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,6 @@
 console.log('sw hit')
 const CACHE_NAME = 'budgetTracker-v1';
+const DATA_CACHE = 'dataCache-v1';
 
 const FILES_TO_CACHE = [
     '/',
@@ -45,6 +46,32 @@ self.addEventListener('activate', (event) => {
     )
 
     self.clients.claim();
+
+});
+
+self.addEventListener('fetch', (event) => {
+
+    if (event.request.url.includes('/api')) {
+        return event.respondWith(
+            caches
+                .open(DATA_CACHE)
+                .then(cache => {
+                    return fetch(event.request)
+                        .then(response => {
+                            if (response.status === 200) {
+                                cache.put(event.request.url, response.clone());
+                            }
+
+                            return response;
+                        })
+                        .catch(err => {
+                            cache.match(event.request);
+                        })
+                })
+                .catch(err => console.log('error fetching api: ', err))
+        );
+    }
+
 
 });
 
