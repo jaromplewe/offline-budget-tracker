a93537c8f1744891bc7d7455f92cc9c3b78ebfdd
Jarom Plewe <jplewe23@gmail.com>
2020-12-02 22:58:35 -0700

    added install listener w/ skipWaitin function

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,29 @@
-console.log('SW REACHED!')
+console.log('sw hit')
+const CACHE_NAME = 'budgetTracker-v1';
+
+const FILES_TO_CACHE = [
+    '/',
+    '/styles.css',
+    '/manifest.webmanifest',
+    '/index.js',
+    '/index.html',
+    '/assets/icons/icon-192x192.png',
+    '/assets/icons/icon-512x512.png'
+]
+
+
+self.addEventListener('install', (event) => {
+    console.log('hit install');
+
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+                return cache.addAll(FILES_TO_CACHE);
+            })
+            .catch(err => console.log('error caching files: ', err))
+    );
+
+    self.skipWaiting();
+});
+
+
+
